5fa0cf5f888ea40b41681e12a32fc4d84d532a22
snahilindoria <snahilindoria999@gmail.com>
2019-01-04 01:39:49 +0530

    commit

---
+++ Live_Streaming_Using_OpenCv.py
@@ -0,0 +1,14 @@
+import numpy
+import cv2
+import requests as np
+
+# install "IP WEBCAM" go get the ip 
+
+
+url = "http://123.5668.325:8080/shot.png"
+
+
+#url connect
+geturl = requests.get(url)
+
+#url photo python load
